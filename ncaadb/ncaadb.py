a247f7e7dbb4f9be5a6f2d5e2aae9e71e4d428bd
Hans Missenheim <hansmissenheim@gmail.com>
2023-06-14 18:41:12 -0400

    Add DB file specific constants

--- ncaadb/ncaadb.py
+++ ncaadb/ncaadb.py
@@ -1,5 +1,10 @@
 from typing import BinaryIO
 
+FILE_HEADER_SIZE = 24
+TABLE_HEADER_SIZE = 40
+TABLE_DEFINITION_SIZE = 8
+TABLE_FIELD_SIZE = 16
+
 
 def read_db(db_file: BinaryIO) -> None:
     """Read an NCAA DB file into python-readable data.
