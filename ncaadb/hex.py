1ead565edd07bd72dcd359762bc14559ffe5caa5
Hans Missenheim <hansmissenheim@gmail.com>
2023-06-21 16:04:06 -0400

    Convert with read_bytes to hex instead of byte list

--- ncaadb/hex.py
+++ ncaadb/hex.py
@@ -4,11 +4,10 @@ def read_string(data: bytes, bits: int, offset: int) -> str:
     return data[start_byte:end_byte].decode("latin-1").replace("\00", "")
 
 
-def read_bytes(record_data: bytes, field: dict[str, int | str]):
-    bites = []
-    for i in range(field.get("bits") // 8):
-        bites.insert(i, record_data[(field.get("offset") // 8) + i])
-    return bites
+def read_bytes(data: bytes, bits: int, offset: int):
+    start_byte = offset // 8
+    end_byte = (offset + bits) // 8
+    return data[start_byte:end_byte].hex()
 
 
 def read_bits(record_data: bytes, offset: int, bits_to_read: int):
